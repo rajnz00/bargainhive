f8883d4a2fc46d6f64ddc6347b08ff9c28880529
rajnz00 <raj-macfarland@xtra.co.nz>
2025-09-18 02:38:16 +1200

    feat(astro): add Astro build pipeline, pages, and Netlify config

---
+++ README-astro.md
@@ -0,0 +1,8 @@
+# BargainHive Astro Upgrade
+
+## Local development
+npm install
+npm run dev
+
+## Build for Netlify
+npm run build
---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://bargainhive.store',
+  output: 'static'
+});
